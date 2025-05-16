afaece5a002e32d5d0207b399fd4f352ae06ac12
sathishkumar63 <ssk@dsmn8.com>
2025-05-16 16:40:23 +0530

    Revert "Delete src/App.tsx"
    This reverts commit 8fdc84b3b90debe87dde9a18363888188eb18fdf.

---
+++ src/App.tsx
@@ -0,0 +1,11 @@
+import { PersonalVoice } from './personal-voice/personal-voice';
+
+function App() {
+  return (
+    <div className="min-h-screen bg-white">
+      <PersonalVoice />
+    </div>
+  );
+}
+
+export default App;
